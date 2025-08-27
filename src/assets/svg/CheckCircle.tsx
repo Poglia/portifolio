e359deb687db164eae2a6b9982a46d18fbfa5e2e
Poglia <pedro.poglia@hotmail.com>
2025-08-27 00:28:12 -0300

    fix: add props on icons to styling on main page

--- src/assets/svg/CheckCircle.tsx
+++ src/assets/svg/CheckCircle.tsx
@@ -1,4 +1,8 @@
-const CheckCircle = () => {
+interface CheckCircleProps {
+    styles: string
+}
+
+const CheckCircle = ({styles}: CheckCircleProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,7 +10,8 @@ const CheckCircle = () => {
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
-      className="size-6"
+      className={styles} 
+      
     >
       <path
         strokeLinecap="round"
