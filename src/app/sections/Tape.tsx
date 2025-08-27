e0a190097a94289385aaedc024e6980786fd51f9
Poglia <pedro.poglia@hotmail.com>
2025-08-27 04:56:52 -0300

    feat: create tape section

--- src/app/sections/Tape.tsx
+++ src/app/sections/Tape.tsx
@@ -1,3 +1,36 @@
+import StarIcon from "@/assets/svg/Star";
+
+const words = [
+  "Performant",
+  "Reliable",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Innovative",
+  "Dynamic",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 lg:py-24 overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word) => (
+              <div key={word} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 uppercase font-extrabold text-sm">
+                  {word}
+                </span>
+                <StarIcon styles="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
