e684fa75db10f6bf8f6d2d442be68ec7a1a6f350
Poglia <pedro.poglia@hotmail.com>
2025-08-29 14:57:46 -0300

    feat: finish contact section at sm, md, and large version

--- src/app/sections/Contact.tsx
+++ src/app/sections/Contact.tsx
@@ -1,3 +1,36 @@
+import ArrowUpRight from "@/assets/svg/ArrowUpRight";
+import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 py-8 px-10 rounded-3xl text-gray-900 text-center md:text-left relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          ></div>
+          <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let&apos;s create something amazing together
+              </h2>
+              <p className="text-sm md:text-base mt-2">
+                Ready to bring your next project to life? Let&apos;s connect and
+                discuss how I can help you achieve your goals.
+              </p>
+            </div>
+            <div className="">
+              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowUpRight />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
