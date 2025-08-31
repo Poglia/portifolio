10d262cc8ccc854862b3426e3f05b9e556976ea7
Poglia <pedro.poglia@hotmail.com>
2025-08-31 16:25:44 -0300

    refactor: hover buttons hero section

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -11,7 +11,7 @@ import SparkleIcon from "@/assets/svg/Sparkle";
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip" id="home">
-      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+      <div className="absolute inset-0 pointer-events-none [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
         <div
           className="absolute inset-0 -z-30 opacity-5"
           style={{
@@ -129,11 +129,11 @@ export const HeroSection = () => {
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4 ">
-          <button className="inline-flex  items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl hover:border-white hover:bg-white hover:text-gray-900 transition duration-700 ease-in-out cursor-pointer">
             <span className="font-semibold ">Explore My Work </span>
             <ArrowDown svgSize="size-4" />
           </button>
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6  hover:bg-inherit hover:text-white hover:border-white/15 transition duration-700 ease-in-out h-12 rounded-xl">
             <span className="">👋</span>
             <span className="font-semibold ">Let&apos;s connect</span>
           </button>
