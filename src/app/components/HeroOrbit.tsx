a9dc01285e3bce75e9fbf9909aa184056a633c05
Poglia <pedro.poglia@hotmail.com>
2025-08-31 08:57:07 -0300

    feat(animate): stars orbit and sin animate

--- src/app/components/HeroOrbit.tsx
+++ src/app/components/HeroOrbit.tsx
@@ -1,22 +1,47 @@
 import Star from "@/assets/svg/Star";
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const HeroOrbit = ({
   children,
   size,
-  rotation
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  rotation,
+  shouldOrbit = false,
+  orbitDuration,
+  shoudSpin = false,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  shouldOrbit?: boolean;
+  orbitDuration?: string;
+  shoudSpin?: boolean;
+  spinDuration?: string;
+}>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
       <div
-         className="flex items-start justify-start" // border border-red-500 
-        style={{
-          transform: `rotate(${rotation}deg)`,	
-          height: `${size}px`,
-          width: `${size}px`,
-        }}
+        className={twMerge(shouldOrbit === true && "animate-spin")}
+        style={{ animationDuration: `${orbitDuration}` }}
       >
-        <div className=" inline-flex" style={{ transform: `rotate(${rotation * -1}deg)`}}>{children}</div>  {/* border border-blue-500 */}
+        <div
+          className="flex items-start justify-start " // border border-red-500
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            height: `${size}px`,
+            width: `${size}px`,
+          }}
+        >
+          <div className={twMerge(shoudSpin === true && "animate-spin")} style={{ animationDuration: `${spinDuration}` }}>
+            <div
+              className=" inline-flex"
+              style={{ transform: `rotate(${rotation * -1}deg)` }}
+            >
+              {children}
+            </div>{" "}
+            {/* border border-blue-500 */}
+          </div>
+        </div>
       </div>
     </div>
   );
