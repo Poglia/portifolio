6dd96ce19ec261de750420d55d00d8bbe82888ae
Poglia <pedro.poglia@hotmail.com>
2025-08-31 07:22:49 -0300

    style(animation): create default config for ping

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -62,7 +62,7 @@ export const HeroSection = () => {
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-large">
             <div className="bg-green-500 size-2.5 rounded-full relative">
-              <div className="bg-green-500 absolute inset-0 rounded-full animate-ping"></div>
+              <div className="bg-green-500 absolute inset-0 rounded-full animate-ping-large"></div>
             </div>
             <div className="text-sm font-medium">
               Available for new projects
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { transform } from "next/dist/build/swc/generated-native";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -11,20 +12,31 @@ export default {
     screens: {
       sm: "375px",
       md: "768px",
-      lg: "1200px"
+      lg: "1200px",
     },
     container: {
       center: true,
       padding: {
         DEFAULT: "1rem",
         md: "2rem",
-      }
+      },
     },
     extend: {
       fontFamily: {
-        sans: 'var(--font-sans)',
-        serif: 'var(--font-serif)',
-      }
+        sans: "var(--font-sans)",
+        serif: "var(--font-serif)",
+      },
+      animation: {
+        "ping-large": "ping-large 1s ease-in-out infinite",
+      },
+      keyframes: {
+        "ping-large": {
+          "75%, 100%": {
+            transform: "scale(3)",
+            opacity: "0",
+          },
+        },
+      },
     },
   },
   plugins: [],
