4c07bf0a3b4db6062080b5a805d25b5b3bfa84a1
Poglia <pedro.poglia@hotmail.com>
2025-09-09 23:21:18 -0300

    fix: config links footer

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -1,27 +1,27 @@
 import ArrowUpRight from "@/assets/svg/ArrowUpRight";
 
 const footerLinks = [
+  // {
+  //   title: "Youtube",
+  //   href: "https://www.youtube.com/@pogliaplays",
+  // },
   {
-    title: "Youtube",
-    href: "#",
-  },
-  {
-    title: "Twitter",
-    href: "#",
+    title: "Github",
+    href: "https://github.com/Poglia"
   },
   {
     title: "Instagram",
-    href: "#",
+    href: "https://www.instagram.com/pedropoglia/",
   },
   {
     title: "Linkedin",
-    href: "#",
+    href: "https://www.linkedin.com/in/pedropoglia/",
   },
 ];
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative overflow-x-clip">
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
@@ -31,7 +31,8 @@ export const Footer = () => {
               <a
                 key={link.title}
                 href={link.href}
-                className="inline-flex items-center gap-1.5"
+                target="_blank"
+                className="inline-flex items-center gap-1.5 cursor-pointer"
               >
                 <span className="font-semibold">{link.title}</span>
                 <ArrowUpRight styles="size-4" />
